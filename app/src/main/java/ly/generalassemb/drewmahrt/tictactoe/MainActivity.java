803410c0052767636cd811967b7772f1d6f83016
changwinstonr <chang.winstonr@gmail.com>
2016-04-27 01:40:49 -0700

    Adds Main fVBID, instantiates inputs, Intents, ends with startActivity.

--- app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/MainActivity.java
+++ app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/MainActivity.java
@@ -5,9 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText playerOne, playerTwo;
+    String playerOneText, playerTwoText, lastWinnarText, playerWinnar;
+    TextView lastWinnar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +22,21 @@ public class MainActivity extends AppCompatActivity {
 
         // BRANCH MAIN
         // find views by ID
+        lastWinnar = (TextView) findViewById(R.id.last_winner_text);
+        playerOne = (EditText) findViewById(R.id.player_one_name);
+        playerTwo = (EditText) findViewById(R.id.player_two_name);
 
         // update them with input
+        playerOneText = new String(playerOne.getText().toString());
+        playerTwoText = new String(playerTwo.getText().toString());
 
         // new Intent with input
+        Intent intent = new Intent(MainActivity.this, GameActivity.class);
+            intent.putExtra("playerOneName", playerOne.getText().toString());
+            intent.putExtra("playerTwoName", playerTwo.getText().toString());
 
         // start game activity with player info (input)
+        startActivity(intent);
 
         //ADD & COMMIT; MERGE
 
