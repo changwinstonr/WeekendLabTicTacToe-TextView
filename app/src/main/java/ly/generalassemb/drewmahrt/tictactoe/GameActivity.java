c19e2d0940af9c929250651d6979f101aefc5327
changwinstonr <chang.winstonr@gmail.com>
2016-04-29 04:09:53 -0700

    Adds shared prefs and intents. fVBID, getIntent, getApplicationContext, and many more inexplicably broken. FUCK THIS. FUCK IT ALL. FUUUUU.

--- app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/GameActivity.java
+++ app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/GameActivity.java
@@ -1,49 +1,62 @@
 package ly.generalassemb.drewmahrt.tictactoe;
 
 import android.content.Intent;
-import android.graphics.Color;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 //import android.widget.ImageView;
 import android.widget.TextView;
-import android.view.View.OnClickListener;
 
-public class GameActivity extends AppCompatActivity implements OnClickListener {
+
+public class GameActivity extends AppCompatActivity implements View.OnClickListener {
     // BRANCH GAME
     //initialize variables
-    private TextView currentTurn, block_1, block_2, block_3, block_4, block_5, block_6, block_7, block_8, block_9;
+    TextView currentTurn, block_1, block_2, block_3, block_4, block_5, block_6, block_7, block_8, block_9;
     TextView[] blockArray;
+    String playerOneWin, playerTwoWin;
 //    ImageView playAgain;
 //                            private Player playerX;
 //                            private Player playerO;
     boolean turn = true;
     // create a counter (int) set it to 0
     //ADD&COMMIT
-    int next_turn = 0;
-    String playerOneWin, playerTwoWin;
+    int nextTurn = 0;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        currentTurn = (TextView) findViewById(R.id.game_message_text);
+
+        currentTurn.findViewById(R.id.game_message_text);
+        //currentTurn = (TextView) findViewById(R.id.game_message_text);
 
         // get intent
         Intent intent = getIntent();
+        currentTurn.setText(getIntent().getExtras().getString("playerX") + " your turn.");
+        currentTurn.setText(getIntent().getExtras().getString("playerO") + " your turn.");
+
         // update the text view with p1 name
-        String playerOneName = intent.getStringExtra("playerOneName");
-        String playerTwoName = intent.getStringExtra("playerTwoName");
+        /*String playerOneName = intent.getStringExtra("playerOneName");
+        String playerTwoName = intent.getStringExtra("playerTwoName");*/
 //                            playerX = new Player(playerOneName);
 //                            playerO = new Player(playerTwoName);
         //ADD&COMMIT
-        currentTurn.setText(getIntent().getExtras().getString("playerO") + " " + "your turn.");
-        currentTurn.setText(getIntent().getExtras().getString("playerX") + " " + "your turn.");
 
         // find all views by ID
         //ADD&COMMIT
-        block_1 = (TextView) findViewById(R.id.textView);
+        block_1.findViewById(R.id.textView);
+        block_2.findViewById(R.id.textView2);
+        block_3.findViewById(R.id.textView3);
+        block_4.findViewById(R.id.textView4);
+        block_5.findViewById(R.id.textView5);
+        block_6.findViewById(R.id.textView6);
+        block_7.findViewById(R.id.textView7);
+        block_8.findViewById(R.id.textView8);
+        block_9.findViewById(R.id.textView9);
+/*        block_1 = (TextView) findViewById(R.id.textView);
         block_2 = (TextView) findViewById(R.id.textView2);
         block_3 = (TextView) findViewById(R.id.textView3);
         block_4 = (TextView) findViewById(R.id.textView4);
@@ -51,55 +64,56 @@ public class GameActivity extends AppCompatActivity implements OnClickListener {
         block_6 = (TextView) findViewById(R.id.textView6);
         block_7 = (TextView) findViewById(R.id.textView7);
         block_8 = (TextView) findViewById(R.id.textView8);
-        block_9 = (TextView) findViewById(R.id.textView9);
+        block_9 = (TextView) findViewById(R.id.textView9);*/
 //        playAgain = (ImageView) findViewById(R.id.playAgain);
 
         blockArray = new TextView[]{
                 block_1, block_2, block_3, block_4, block_5,
                 block_6, block_7, block_8, block_9};
 
-        for (TextView action : blockArray){
+        /*for (TextView action : blockArray){
             action.setOnClickListener(this);
-        }
-
+        }*/
+        //replay drawable OnClick
         /*playAgain.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
                 turn = true;
-                next_turn = 0;
+                nextTurn = 0;
                 //replay(true);
             }
         });*/
-
+    }//end onCreate
+    @Override
+    public void onClick(View v) {
+        TextView markBlock = (TextView) v;
+        blockSelect(markBlock);
     }
-        @Override
-        public void onClick(View v) {
-            TextView markBlock = (TextView) v;
-            blockSelect (markBlock);
+    // on each click listener :
+    //1. if counter is 0 or even number input an "X" to that text view using setText()
+    //2. if counter is odd number input an "X" to that text view using setText()
+    // set all on click listeners for each view
+    //ADD&COMMIT
+    public void blockSelect(TextView markBlock){
+        if(turn){
+            currentTurn.setText(getIntent().getExtras().getString("playerX")+" 's \nturn!");
+            //currentTurn.setTextColor(Color.WHITE); //was supposed to go with activity_game black background
+            markBlock.setText("X");
+        }else{
+            currentTurn.setText(getIntent().getExtras().getString("playerO")+" 's \nturn!");
+            //currentTurn.setTextColor(Color.WHITE); //was supposed to go with activity_game black background
+            markBlock.setText("O");
+        }
+        //increment the counter
+        nextTurn++;
+        markBlock.setClickable(false);
+        //bounces turns
+        turn = !turn;
+        checkWin();
+    }
 
-        }
-        // on each click listener :
-        //1. if counter is 0 or even number input an "X" to that text view using setText()
-        //2. if counter is odd number input an "X" to that text view using setText()
-        // set all on click listeners for each view
-        //ADD&COMMIT
-        public void blockSelect(TextView markBlock){
-            if(turn){
-                currentTurn.setText(getIntent().getExtras().getString("playerX")+" 's \nturn!");
-                currentTurn.setTextColor(Color.WHITE);
-                markBlock.setText("X");
-            }else{
-                currentTurn.setText(getIntent().getExtras().getString("playerO")+" 's \nturn!");
-                currentTurn.setTextColor(Color.WHITE);
-                markBlock.setText("O");
-            }
-            //increment the counter
-            next_turn++;
-            markBlock.setClickable(false);
-            turn = !turn;
-            checkWin();
-        }
-        //ADD&COMMIT
+
+    //ADD&COMMIT
 
 
         // check for a win
@@ -114,49 +128,62 @@ public class GameActivity extends AppCompatActivity implements OnClickListener {
         // 1 2 3
         // 4 5 6
         // 7 8 9
-        //horiz
+        //#winHoriz
         if (block_1.getText() == block_2.getText()
                 && block_2.getText() == block_3.getText()
                 && !block_1.isClickable())
-                winnar = true;
+                    winnar = true;
         else if (block_4.getText() == block_5.getText()
                 && block_5.getText() == block_6.getText()
                 && !block_4.isClickable())
-                winnar = true;
+                    winnar = true;
         else if (block_7.getText() == block_8.getText()
                 && block_8.getText() == block_9.getText()
                 && !block_7.isClickable())
-                winnar = true;
-        //vert
+                    winnar = true;
+        //#winVert
         if (block_1.getText() == block_4.getText()
                 && block_4.getText() == block_7.getText()
                 && !block_1.isClickable())
-                winnar = true;
+                    winnar = true;
         else if (block_2.getText() == block_5.getText()
                 && block_5.getText() == block_8.getText()
                 && !block_4.isClickable())
-                winnar = true;
+                    winnar = true;
         else if (block_3.getText() == block_6.getText()
                 && block_6.getText() == block_9.getText()
                 && !block_3.isClickable())
-                winnar = true;
-        //diag
+                    winnar = true;
+        //#winDiag
         if (block_1.getText() == block_5.getText()
                 && block_5.getText() == block_9.getText()
                 && !block_1.isClickable())
-                winnar = true;
+                    winnar = true;
         else if (block_3.getText() == block_5.getText()
                 && block_5.getText() == block_7.getText()
                 && !block_3.isClickable())
-                winnar = true;
+                    winnar = true;
 
-   /*     if (winnar){
-            if (!turn){
-                currentTurn.setText(getIntent().getExtras().getString("playerO")+" is a winnar!");
-                playerOneWin = getIntent().getExtras().getString("")
+        if (winnar) {
+            //who won? display on activity_main
+            SharedPreferences sharedPreferences =
+                    getApplicationContext().getSharedPreferences (MainActivity.PREFERENCES_KEY, MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
 
-               */
+            if (!turn) {
+                currentTurn.setText(getIntent().getExtras().getString("playerO") + " is a winnar!");
+                playerOneWin = getIntent().getExtras().getString("");
+                editor.putString("winnar", playerOneWin);
+                editor.apply(); //NTS: very important; OT: syntax diff ".apply();" vs ".commit();"?
+
+            } else {
+                currentTurn.setText(getIntent().getExtras().getString("playerX") + " is a winnar!");
+                playerTwoWin = getIntent().getExtras().getString("");
+                editor.putString("winnar", playerTwoWin);
+                editor.apply();
             }
+        }
+    }
 }
 
 
--- app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/MainActivity.java
+++ app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/MainActivity.java
@@ -1,6 +1,7 @@
 package ly.generalassemb.drewmahrt.tictactoe;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,44 +12,54 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText playerOne, playerTwo;
-    //private TextView lastWinnar;
-    Button startGame;
-    String playerName1, playerName2;
+    EditText playerOne, playerTwo;
+    TextView lastWinnar;
+    //Button startGame;
+    String playerName1, playerName2, winnar;
+
+    //call prefs shared
+    public static final String PREFERENCES_KEY = "ly.generalassemb.drewmahrt.tictactoe.prefs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+    }
         // BRANCH MAIN
         // find views by ID
+    public void startGameClick (View view){
         playerOne = (EditText) findViewById(R.id.player_one_name);
         playerTwo = (EditText) findViewById(R.id.player_two_name);
-        //lastWinnar = (TextView) findViewById(R.id.last_winner_text);
-        startGame = (Button) findViewById(R.id.start_game_button);
+        //startGame = (Button) findViewById(R.id.start_game_button);
 
-        startGame.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick (View v){
-                // update them with input
-                playerName1 = playerOne.getText().toString();
-                playerName2 = playerTwo.getText().toString();
-//        playerOne = new String(playerOne.getText().toString());
-//        playerTwo = new String(playerTwo.getText().toString());
+        //startGame.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+            // update them with input
+        playerName1 = new String(playerOne.getText().toString());
+        playerName2 = new String(playerTwo.getText().toString());
 
-                // new Intent with input
-                Intent intent = new Intent(MainActivity.this, GameActivity.class);
-                intent.putExtra("playerOneName", playerOne.getText().toString());
-                intent.putExtra("playerTwoName", playerTwo.getText().toString());
+            // new Intent with input
+            Intent intent = new Intent(MainActivity.this, GameActivity.class);
+            intent.putExtra("playerOneName", playerOne.getText().toString());
+            intent.putExtra("playerTwoName", playerTwo.getText().toString());
+            // start game activity with player info (input)
+            startActivity(intent);
+        }
+        //get winner of TTT share dem prefs
+        //implement SHARED PREFS after GameActivity completion
+        @Override
+        protected void onResume(){
+            super.onResume();
+            SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(PREFERENCES_KEY, MODE_PRIVATE);
+            sharedPreferences.getString("winnar","");
+            lastWinnar = (TextView) findViewById(R.id.last_winner_text);
+            winnar = String.valueOf(sharedPreferences.getString("winnar",""));
+            lastWinnar.setText(winnar);
+        }
 
-                // start game activity with player info (input)
-                startActivity(intent);
-            }
 
-        });
     }
 
 
-    //implement SHARED PREFS after GameActivity completion
-}
+
+
